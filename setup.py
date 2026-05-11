a41b667b1934bcec0b7a13c31a5b5a18a1f6b6a9
mohitthekeeda456 <mohitthekeeda456@gmail.com>
2026-05-11 15:52:13 +0530

    setup.py file ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+ 
+def get_requirements()->List[str]:
+    "this function will return list of requiremnets"
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    
+    return requirement_lst
+
+print(get_requirements())
+
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Mohit Kumar Gola",
+    author_email="mohitthekeeda456@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
